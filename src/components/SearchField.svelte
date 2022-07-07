8e31cf11bd61e9d9f543e238f0b4c3a63686c3da
Bernhard Häussermann <bernhardgh@icloud.com>
2022-07-07 21:26:59 +0200

    Add search text box to employees page

---
+++ src/components/SearchField.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+  import Textfield from '@smui/textfield';
+  import Icon from '@smui/textfield/icon';
+
+  export let value = '';
+</script>
+
+<Textfield
+  class="search-field"
+  variant="outlined"
+  label="Search"
+  bind:value
+  on:input>
+  <Icon class="material-icons" slot="trailingIcon">search</Icon>
+</Textfield>
--- src/pages/Employees.svelte
+++ src/pages/Employees.svelte
@@ -1,38 +1,52 @@
 <script lang="ts">
   import { onMount } from 'svelte';
+
   import DataTable, { Head, Body, Row, Cell, Label, SortValue } from '@smui/data-table';
-  import IconButton from '@smui/icon-button';
   import LinearProgress from '@smui/linear-progress';
+  import IconButton from '@smui/icon-button';
+  import SearchField from '../components/SearchField.svelte';
+  
   import type { Employee } from '../models/employee';
   import employeesService from '../services/employees-service';
 
   let didLoad = false;
   let errorMessage: string = null;
   let employees: Employee[] = [];
-  let sortedEmployees: Employee[] = [];
+  let displayedEmployees: Employee[] = [];
   let sortProperty: keyof Employee = 'lastName';
   let sortDirection: SortValue = SortValue.ASCENDING;
+  let filterText = '';
 
   onMount(async () => {
     try {
       employees = await employeesService.getAll();
-      refreshSortedEmployees();
+      refreshDisplayedEmployees();
       didLoad = true;
     } catch (error) {
       errorMessage = 'Error retrieving employees: ' + error.message;
     }
   });
 
-  function refreshSortedEmployees() {
-    sortedEmployees = [...employees];
-    sortedEmployees.sort((employee1, employee2) => {
+  function refreshDisplayedEmployees() {
+    displayedEmployees = employees.filter(e =>
+      !filterText
+      || (e.lastName.toLowerCase().indexOf(filterText) !== -1)
+      || (e.firstName.toLowerCase().indexOf(filterText) !== -1)
+      || (e.title.toLowerCase().indexOf(filterText) !== -1));
+    displayedEmployees.sort((employee1, employee2) => {
       const compareValue = employee1[sortProperty].localeCompare(employee2[sortProperty]);
       return sortDirection === SortValue.ASCENDING ? compareValue : -compareValue;
     });
   }
 </script>
 
-<h2>Employees</h2>
+<div class="title-row">
+  <h2 class="title">Employees</h2>
+
+  {#if didLoad}
+  <SearchField bind:value={filterText} on:input={refreshDisplayedEmployees} />
+  {/if}
+</div>
 
 {#if errorMessage}
 <div>{errorMessage}</div>
@@ -41,7 +55,7 @@
   sortable
   bind:sort={sortProperty}
   bind:sortDirection
-  on:SMUIDataTable:sorted={refreshSortedEmployees}
+  on:SMUIDataTable:sorted={refreshDisplayedEmployees}
   style="width: 100%;">
   <Head>
     <Row>
@@ -60,7 +74,7 @@
     </Row>
   </Head>
   <Body>
-    {#each sortedEmployees as employee}
+    {#each displayedEmployees as employee}
     <Row>
       <Cell>{employee.lastName}</Cell>
       <Cell>{employee.firstName}</Cell>
@@ -72,3 +86,20 @@
   <LinearProgress indeterminate bind:closed={didLoad} slot="progress" />
 </DataTable>
 {/if}
+
+<style>
+  .title-row {
+    display: flex;
+    margin-top: 20px;
+    margin-bottom: 20px;
+  }
+
+  .title-row > * {
+    margin-top: auto;
+    margin-bottom: auto;
+  }
+
+  .title {
+    flex-grow: 1;
+  }
+</style>
