f127e8b77cb9a8933660611b01fe378e4ae2c353
Bernhard Häussermann <bernhardgh@icloud.com>
2022-07-07 17:46:14 +0200

    Make employee columns sortable

--- src/models/employee.ts
+++ src/models/employee.ts
@@ -1,5 +1,5 @@
 export interface Employee {
-  firstName: string;
   lastName: string;
+  firstName: string;
   title: string;
 }
--- src/pages/Employees.svelte
+++ src/pages/Employees.svelte
@@ -1,22 +1,35 @@
 <script lang="ts">
   import { onMount } from 'svelte';
-  import DataTable, { Head, Body, Row, Cell } from '@smui/data-table';
+  import DataTable, { Head, Body, Row, Cell, Label, SortValue } from '@smui/data-table';
+  import IconButton from '@smui/icon-button';
   import LinearProgress from '@smui/linear-progress';
   import type { Employee } from '../models/employee';
   import employeesService from '../services/employees-service';
 
   let didLoad = false;
-  let employees: Employee[] = [];
   let errorMessage: string = null;
+  let employees: Employee[] = [];
+  let sortedEmployees: Employee[] = [];
+  let sortProperty: keyof Employee = 'lastName';
+  let sortDirection: SortValue = SortValue.ASCENDING;
 
   onMount(async () => {
     try {
       employees = await employeesService.getAll();
+      refreshSortedEmployees();
       didLoad = true;
     } catch (error) {
       errorMessage = 'Error retrieving employees: ' + error.message;
     }
   });
+
+  function refreshSortedEmployees() {
+    sortedEmployees = [...employees];
+    sortedEmployees.sort((employee1, employee2) => {
+      const compareValue = employee1[sortProperty].localeCompare(employee2[sortProperty]);
+      return sortDirection === SortValue.ASCENDING ? compareValue : -compareValue;
+    });
+  }
 </script>
 
 <h2>Employees</h2>
@@ -24,16 +37,30 @@
 {#if errorMessage}
 <div>{errorMessage}</div>
 {:else}
-<DataTable style="width: 100%;">
+<DataTable
+  sortable
+  bind:sort={sortProperty}
+  bind:sortDirection
+  on:SMUIDataTable:sorted={refreshSortedEmployees}
+  style="width: 100%;">
   <Head>
     <Row>
-      <Cell>Last name</Cell>
-      <Cell>First name</Cell>
-      <Cell>Title</Cell>
+      <Cell columnId="lastName">
+        <Label>Last name</Label>
+        <IconButton class="material-icons">arrow_upward</IconButton>
+      </Cell>
+      <Cell columnId="firstName">
+        <Label>First name</Label>
+        <IconButton class="material-icons">arrow_upward</IconButton>
+      </Cell>
+      <Cell columnId="title">
+        <Label>Title</Label>
+        <IconButton class="material-icons">arrow_upward</IconButton>
+      </Cell>
     </Row>
   </Head>
   <Body>
-    {#each employees as employee}
+    {#each sortedEmployees as employee}
     <Row>
       <Cell>{employee.lastName}</Cell>
       <Cell>{employee.firstName}</Cell>
